0d76e9021105c1c1e235b6104e459c0628818061
Edward Shanahan <edwardshanahan357@gmail.com>
2023-02-08 21:42:58 +0000

    Add code to check if username is in localstorage If username is in localstorage todo section will display. If username doesn’t exists profile section will display

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,19 @@ let dateElement = document.getElementById("date");
 let addTodoForm = document.getElementById("add-to-form");
 let todosLeftElement = document.getElementById("todo-left");
 let clearAllElement = document.getElementById("clear-all");
+
+/**
+ * Checks if username exists in localstorage.
+ * if username is in localstorage, profile won't display 
+ * and todo section will display
+ */
+
+window.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.getItem("username")) {
+        profileElement.classList.add("hide");
+
+        todoSectionElement.classList.remove("hide");
+    } else {
+        todoSectionElement.classList.add("hide");
+    }
+})
