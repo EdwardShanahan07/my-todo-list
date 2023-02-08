324843d3b917a99864af76136a7e664d05482a8a
Edward Shanahan <edwardshanahan357@gmail.com>
2023-02-08 21:14:56 +0000

    Add DOM elements to variables

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+// DOM Elements
+let profileElement = document.getElementById("profile");
+let usernameForm = document.getElementById("get-username");
+let todoSectionElement = document.getElementById("todo-section");
+let dateElement = document.getElementById("date");
+let addTodoForm = document.getElementById("add-to-form");
+let todosLeftElement = document.getElementById("todo-left");
+let clearAllElement = document.getElementById("clear-all");
